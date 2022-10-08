3bbdd15316773ff74dfcedd9fbec0e1a1cc52dea
Nandini928 <114830857+Nandini928@users.noreply.github.com>
2022-10-08 14:02:36 -0700

    changed

---
+++ project 1/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args){
+        System.out.println();
+
+        int sum = 0;
+        for (int i = 1; i <= 5; i++) {
+            sum = sum + i;
+            System.out.println(sum);
+        }
+
+    }
+}
